9d9cbf1c33f8e0c51b8263b0dcc50bdd4e0aa2dd
Anthony Lee <antwonlee@gmail.com>
2021-10-04 14:54:46 -0500

    feat: Add basic hard hat configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,47 +1,39 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   defaultNetwork: "rinkeby",
   networks: {
-    hardhat: {
-    },
+    hardhat: {},
     rinkeby: {
-      url: "https://eth-mainnet.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
-      accounts: [privateKey1, privateKey2, ...]
-    }
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_RINKEBY_ID}`,
+      accounts: [`${process.env.ACCOUNT_PRIVATE_KEY}`],
+    },
   },
   solidity: {
     version: "0.8.4",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts"
+    artifacts: "./artifacts",
   },
   mocha: {
-    timeout: 20000
-  }
+    timeout: 20000,
+  },
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Library = await hre.ethers.getContractFactory("Library");
+  const library = await Library.deploy();
+
+  await library.deployed();
+
+  console.log("Library Contrat deployed to:", library.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
